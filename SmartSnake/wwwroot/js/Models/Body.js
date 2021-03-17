4d0671020cd208c186327a6aa1b333289a3f5940
Proger2k <stolbetskiy.yura@gmail.com>
2021-03-17 14:28:59 +0300

    Added class Body with method Move

---
+++ SmartSnake/wwwroot/js/Models/Body.js
@@ -0,0 +1,24 @@
+﻿class Body
+{
+    constructor(head, length, bodyWidth, coordinates)
+    {
+        this.head = head;
+        this.length = length;
+        this.bodyWindth = bodyWidth;
+        this.coordinates = coordinates;
+    } 
+    
+    Move(direction)
+    {
+        for (let i = 0; i < this.length - 2; i++)
+        {
+            this.coordinates[i].X = this.coordinates[i + 1].X;
+            this.coordinates[i].Y = this.coordinates[i + 1].Y;
+        }
+        
+        this.coordinates[this.length - 1].X = this.head.coordinates.X;
+        this.coordinates[this.length - 1].Y = this.head.coordinates.Y;
+        
+        this.head.Move(direction);
+    }
+}
