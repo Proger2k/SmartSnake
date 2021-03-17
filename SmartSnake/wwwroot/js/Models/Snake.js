0121fbec037badd44a39d1bb0c9d7172a1c925f1
Proger2k <stolbetskiy.yura@gmail.com>
2021-03-17 20:20:45 +0300

    Added method Draw, added styles for snake

--- SmartSnake/wwwroot/js/Models/Snake.js
+++ SmartSnake/wwwroot/js/Models/Snake.js
@@ -6,4 +6,21 @@ class Snake
         this.body = body;
     }
     
+    Intervals()
+    {
+        this.snakeMove = setInterval(() => {
+            
+            this.head.Move(-1);
+        }, fps);
+    }
+    
+    Draw()
+    {
+        for(let i = 0; i < this.body.length; i++)
+        {
+            gameZone.innerHTML += `<div class="body" style="left: ${this.body.coordinates[i].X}px; top: ${this.body.coordinates[i].Y}px;"></div>`
+        }
+        
+        gameZone.innerHTML += `<div class="head" style="left: ${this.head.coordinates.X}px; top: ${this.head.coordinates.Y}px;"></div>`
+    }
 }
--- SmartSnake/wwwroot/js/index.js
+++ SmartSnake/wwwroot/js/index.js
@@ -1,1 +1,17 @@
-﻿movement();
+﻿fps = 1000/60;
+let gameZone = document.querySelector('.game-zone')
+movement();
+
+let head = new Head(Math.PI/2,5,Math.PI/50, {X: 100, Y: 100});
+let coordinatesBody = new Array(30);
+for(let i = 0; i < coordinatesBody.length; i++)
+{
+    coordinatesBody[i] = {X: 100, Y: 105 + i*5};
+}
+
+let body = new Body(head, 30,45, coordinatesBody);
+
+let snake = new Snake(head, body);
+
+snake.Draw();
+
