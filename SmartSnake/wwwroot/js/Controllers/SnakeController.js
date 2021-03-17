d5dc936eeaa765ff05d5a9add4eeb0a45d4f7da2
Proger2k <stolbetskiy.yura@gmail.com>
2021-03-17 15:25:18 +0300

    Fixed bugs, added contoller SnakeController, added main file index

---
+++ SmartSnake/wwwroot/js/Controllers/SnakeController.js
@@ -0,0 +1,24 @@
+﻿function movement() 
+{
+    document.addEventListener("keydown", (e) => {
+        switch (e.keyCode) {
+            case 39:
+                // right
+                
+                break
+            case 37:
+                // left
+                break
+        }
+    })
+
+    document.addEventListener("keyup", (e) => {
+        switch (e.keyCode) {
+            case 39:
+                break
+
+            case 37:
+                break
+        }
+    })
+}
