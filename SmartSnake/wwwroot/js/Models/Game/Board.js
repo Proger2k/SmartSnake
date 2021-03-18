be51146710902200d7670131ba06438e3a2e6f7f
Proger2k <stolbetskiy.yura@gmail.com>
2021-03-18 21:10:43 +0300

    Added methods FoodInitialization, SnakesInitialization and ControlInitialization.

--- SmartSnake/wwwroot/js/Models/Game/Board.js
+++ SmartSnake/wwwroot/js/Models/Game/Board.js
@@ -13,33 +13,52 @@
 
     Initialization()
     {
-        let x = this.GetRandomInt(this.width);
-        let y = this.GetRandomInt(this.height);
-        let direction = this.GetRandomDirection();
+        this.FoodInitialization();
         
-        let head = new Head(direction, this.snakeSpeed, this.headTurningSpeed, { X: x, Y: y });
-
-        let coordinatesBody = new Array(this.snakeLength);
-        for(let i = 0; i < coordinatesBody.length; i++)
-        {
-            coordinatesBody[i] = {X: x, Y: y};
-        }
-
-        let body = new Body(head, this.snakeLength, coordinatesBody);
-
-        let snake = new Snake(head, body);
-
+        let snakes = this.SnakesInitialization();
+        this.ControlInitialization(snakes);
+    }
+    
+    FoodInitialization()
+    {
         let apple = new Apple(this.numberOfApples, this.height, this.width);
         apple.Initialization();
+    }
+    
+    SnakesInitialization()
+    {
+        let snakes = new Array(this.numberOfSnakes);
         
-        snake.Draw();
+        for (let i = 0; i < this.numberOfSnakes; i++)
+        {
+            let x = this.GetRandomInt(this.width);
+            let y = this.GetRandomInt(this.height);
+            let direction = this.GetRandomDirection();
+
+            let head = new Head(direction, this.snakeSpeed, this.headTurningSpeed, { X: x, Y: y });
+
+            let coordinatesBody = new Array(this.snakeLength);
+            for(let i = 0; i < coordinatesBody.length; i++)
+            {
+                coordinatesBody[i] = {X: x, Y: y};
+            }
+
+            let body = new Body(head, this.snakeLength, coordinatesBody);
+
+            snakes[i] = new Snake(head, body);
+
+            snakes[i].Draw();
+        }
         
-        let snakes = new Array(1);
-        snakes[0] = snake;
-         let interval = new Interval(snakes);
-         interval.Move();
-        
-        let snakeController = new SnakeController(snake);
+        return snakes;
+    }
+
+    ControlInitialization(snakes)
+    {
+        let interval = new Interval(snakes);
+        interval.Move();
+
+        let snakeController = new SnakeController(snakes[0]);
         snakeController.Movement();
     }
 
