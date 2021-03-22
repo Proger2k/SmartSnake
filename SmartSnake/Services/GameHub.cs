1eb92205770466c9a5c54c1c2b5d252a871395f7
Proger2k <stolbetskiy.yura@gmail.com>
2021-03-22 13:54:31 +0300

    GameHub class changed

--- SmartSnake/Services/GameHub.cs
+++ SmartSnake/Services/GameHub.cs
@@ -1,9 +1,32 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
 
 namespace SmartSnake
 {
     public class GameHub : Hub
     {
-        
+        public async Task Send(GameHubModel model)
+        {
+            model.ConnectionId = Context.ConnectionId;
+            await Clients.Others.SendAsync("Receive", model);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.Others.SendAsync("Notify", $"{Context.ConnectionId}", "1");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Clients.All.SendAsync("Notify", $"{Context.ConnectionId}", "-1");
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+
+    public class GameHubModel
+    {
+        public string ConnectionId { get; set; }
     }
 }
