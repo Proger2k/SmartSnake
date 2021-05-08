16b04298cf80f7d4081b52868a0fe52e145f74ff
Proger2k <stolbetskiy.yura@gmail.com>
2021-05-08 15:38:50 +0300

    Added registration model

--- SmartSnake/Pages/Account/Registration.cshtml.cs
+++ SmartSnake/Pages/Account/Registration.cshtml.cs
@@ -1,9 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SmartSnake.Pages.Account
 {
     public class Registration : PageModel
     {
+        
+        public class RegisterViewModel
+        {
+            [Required]
+            public string Name { get; set; }
+
+            [Required]
+            public string Email { get; set; }
+
+            [Required]
+            public int Year { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [Required]
+            [Compare("Password", ErrorMessage = "Password mismatch")]
+            [DataType(DataType.Password)]
+            public string PasswordConfirm { get; set; }
+        }
         public void OnGet()
         {
             
