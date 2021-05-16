f56c41ee6fa04eb7d35ef75b9df845047ae30c11
Proger2k <stolbetskiy.yura@gmail.com>
2021-05-16 21:25:04 +0300

    Added the Rating page

---
+++ SmartSnake/Pages/Game/Rating.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SmartSnake.Pages.Game
+{
+    public class Rating : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
